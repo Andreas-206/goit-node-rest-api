ddea73e02ca7ccdb7c30c5b5aa376182f1600587
Andreas-206 <puma_real@ukr.net>
2024-04-29 15:38:17 +0300

    +

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,5 +1,4 @@
 import HttpError from '../helpers/HttpError.js'
-import wrapAsync from '../helpers/wrapAsync.js'
 import {
 	listContacts,
 	addContact,
@@ -8,53 +7,60 @@ import {
 	updateContactById,
 } from '../services/contactsServices.js'
 
-export const getAllContacts = wrapAsync(async (req, res) => {
-	const result = await listContacts()
-	res.json(result)
-})
-
-export const getOneContact = wrapAsync(async (req, res) => {
-	const { id } = req.params
-	const result = await getContactById(id)
-	if (!result) {
-		throw HttpError(404)
+export const getAllContacts = async (req, res, next) => {
+	try {
+		const result = await contactService.listContacts()
+		res.json(result)
+	} catch (error) {
+		next({})
 	}
+}
 
-	res.json(result)
-})
+export const getOneContact = async (req, res, next) => {
+	try {
+		const { id } = req.params
+		const result = await getContactById(id)
+		if (!result) throw HttpError(404)
 
-export const deleteContact = wrapAsync(async (req, res) => {
-	const { id } = req.params
-	const result = await removeContact(id)
-	if (!result) {
-		throw HttpError(404)
+		res.json(result)
+	} catch (error) {
+		next(error.status ? error : {})
 	}
-	res.json(result)
-})
+}
 
-export const createContact = wrapAsync(async (req, res) => {
-	const result = await addContact(req.body)
-	res.status(201).json(result)
-})
+export const deleteContact = async (req, res, next) => {
+	try {
+		const { id } = req.params
+		const result = await removeContact(id)
+		if (!result) throw HttpError(404)
+		res.json(result)
+	} catch (error) {
+		next(error.status ? error : {})
+	}
+}
 
-export const updateContact = wrapAsync(async (req, res) => {
-	const { id } = req.params
-	const { name, email, phone } = req.body
-	if (!name && !email && !phone) {
-		return res
-			.status(400)
-			.json({ message: 'Body must have at least one field' })
+export const createContact = async (req, res, next) => {
+	try {
+		const { name, email, phone } = req.body
+		const result = await addContact(name, email, phone)
+		res.status(201).json(result)
+	} catch (error) {
+		next({})
 	}
-	const existingContact = await getContactById(id)
-	const updatedContact = {
-		id,
-		name: name || existingContact.name,
-		email: email || existingContact.email,
-		phone: phone || existingContact.phone,
+}
+
+export const updateContact = async (req, res, next) => {
+	try {
+		if (Object.keys(req.body).length === 0)
+			throw HttpError(400, 'Body must have at least one field')
+
+		const { id } = req.params
+		const result = await updateContactById(id, req.body)
+
+		if (!result) throw HttpError(404)
+
+		res.json(result)
+	} catch (error) {
+		next(error.status ? error : {})
 	}
-	const result = await updateContactById(id, updatedContact)
-	if (!result) {
-		throw HttpError(404)
-	}
-	res.json(result)
-})
+}
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -5,3 +5,9 @@ export const createContactSchema = Joi.object({
 	email: Joi.string().email().required(),
 	phone: Joi.string().required(),
 })
+
+export const updateContactSchema = Joi.object({
+	name: Joi.string().required(),
+	email: Joi.string().email().required(),
+	phone: Joi.string().required(),
+})
