a48d728e63dc6fa6a09c5e11c9c43ba1288087b6
Andreas-206 <puma_real@ukr.net>
2024-05-03 21:33:14 +0300

    Update contactsControllers.js

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -18,8 +18,8 @@ export const getAllContacts = async (req, res, next) => {
 }
 
 export const getOneContact = async (req, res, next) => {
+	const { id } = req.params
 	try {
-		const { id } = req.params
 		const result = await Contact.findById(id)
 		if (!result) throw HttpError(404)
 
@@ -30,8 +30,8 @@ export const getOneContact = async (req, res, next) => {
 }
 
 export const deleteContact = async (req, res, next) => {
+	const { id } = req.params
 	try {
-		const { id } = req.params
 		const result = await Contact.findByIdAndDelete(id)
 		if (!result) throw HttpError(404)
 		res.json(result)
@@ -50,11 +50,10 @@ export const createContact = async (req, res, next) => {
 }
 
 export const updateContact = async (req, res, next) => {
+	const { id } = req.params
 	try {
 		if (Object.keys(req.body).length === 0)
 			throw HttpError(400, 'Body must have at least one field')
-
-		const { id } = req.params
 		const result = await Contact.findByIdAndUpdate(id, req.body, { new: true })
 
 		if (!result) throw HttpError(404)
@@ -66,8 +65,8 @@ export const updateContact = async (req, res, next) => {
 }
 
 export const updateStatusContact = async (req, res, next) => {
+	const { id } = req.params
 	try {
-		const { id } = req.params
 		const result = await Contact.findByIdAndUpdate(id, req.body, { new: true })
 		if (!result) throw HttpError(404)
 
