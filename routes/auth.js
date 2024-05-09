104378f5d587c854a71c6ab11d1a9cd8160e1a73
Andreas-206 <puma_real@ukr.net>
2024-05-09 16:24:25 +0300

    +

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { register } from '../controllers/authControllers.js'
+import { register, login } from '../controllers/authControllers.js'
 import validateBody from '../helpers/validateBody.js'
-import { registerSchema } from '../schemas/user.js'
+import { registerSchema, loginSchema } from '../schemas/user.js'
 
 const authRouter = express.Router()
 
-authRouter.post('/users/register', validateBody(registerSchema), register)
+authRouter.post('/register', validateBody(registerSchema), register)
+authRouter.post('/login', validateBody(loginSchema), login)
 
 export default authRouter
--- schemas/user.js
+++ schemas/user.js
@@ -28,13 +28,15 @@ const userSchema = new mongoose.Schema(
 	}
 )
 
+const emailRegexp = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+
 export const registerSchema = Joi.object({
-	email: Joi.string().email().required(),
+	email: Joi.string().pattern(emailRegexp).required(),
 	password: Joi.string().required().min(6),
 })
 
 export const loginSchema = Joi.object({
-	email: Joi.string().email().required(),
+	email: Joi.string().pattern(emailRegexp).required(),
 	password: Joi.string().required().min(6),
 })
 
