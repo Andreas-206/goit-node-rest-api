9e85cd0f308afc1ed7942250510f33347de4dbf2
Andreas-206 <puma_real@ukr.net>
2024-05-10 20:25:10 +0300

    +fin1

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,28 +1,6 @@
 import jwt from 'jsonwebtoken'
 import User from '../schemas/user.js'
 
-// function authorization(req, res, next) {
-// 	const authorizationHeader = req.headers.authorization
-// 	if (typeof authorizationHeader !== 'undefined') {
-// 		return res.status(401).send({ message: 'Not authorized' })
-// 	}
-
-// 	const [bearer, token] = authorizationHeader.split(' ', 2)
-
-// 	if (bearer !== 'Bearer') {
-// 		return res.status(401).send({ message: 'Not authorized' })
-// 	}
-
-// 	jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-// 		if (err) {
-// 			return res.status(401).send({ message: 'Invalid token' })
-// 		}
-// 		req.user = { _id: decoded.id, name: decoded.name }
-// 		next()
-// 	})
-// }
-// export default authorization
-
 function authorization(req, res, next) {
 	const authorizationHeader = req.headers.authorization
 
