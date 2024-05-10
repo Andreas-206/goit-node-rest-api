efa02a09aa58a47759223823fe77c83ddba6b5f8
Andreas-206 <puma_real@ukr.net>
2024-05-10 19:49:23 +0300

    Update authControllers.js

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,5 +1,4 @@
 import User from '../schemas/user.js'
-import HttpError from '../helpers/HttpError.js'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
@@ -12,7 +11,7 @@ export const register = async (req, res, next) => {
 
 	const hashPassword = await bcrypt.hash(password, 10)
 	try {
-		await user.create({ email, password: hashPassword })
+		await User.create({ email, password: hashPassword })
 
 		res.status(201).send('Register new User.')
 	} catch (error) {
